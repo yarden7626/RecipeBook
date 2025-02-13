1698eef39fc2d5dcc2754492a950fdafd6d0176c
Zur <yarden7671@gmail.com>
2025-02-13 22:11:03 +0200

    commit all

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                startActivity(intent); //עובר למסך הכניסה
             }
         });
     }
