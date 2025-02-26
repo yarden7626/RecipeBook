b03174e9778cdd149dd95dd01d7810028fd85a59
Zur <yarden7671@gmail.com>
2025-02-26 15:15:57 +0200

    יצרתי קובץ DateManger והגדרתי בו פעולות

---
+++ app/src/main/java/com/example/recipebook/DataManger.java
@@ -0,0 +1,21 @@
+package com.example.recipebook;
+
+import java.util.ArrayList;
+
+public class DataManger {
+
+    public Recipe getInfo(int index) //שולף מידע ממתכון לפי אינדקס המתכון שהתקבל
+    {
+
+    }
+
+    public Recipe getInfo(String name) //שולף מידע ממתכון לפי שם המתכון שהתקבל
+    {
+
+    }
+
+    public ArrayList<Recipe> getInfo(String userID) //שולף מידע של מתכון מרשימת מתכונים לפי התז מתכון שהתקבל
+    {
+
+    }
+}
