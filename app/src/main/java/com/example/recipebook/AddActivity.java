84d585df8c45491ec592a1e09f6943983d4b1c87
Zur <yarden7671@gmail.com>
2025-03-31 18:23:52 +0300

    שיניתי שכלוחצים על בחירת זמן לטיימר הטיימר יפתח

--- app/src/main/java/com/example/recipebook/AddActivity.java
+++ app/src/main/java/com/example/recipebook/AddActivity.java
@@ -267,6 +267,7 @@ public class AddActivity extends AppCompatActivity {
                 timerDuration // timerDuration
         );
 
+
         // שמירת המתכון בבסיס הנתונים
         new Thread(() -> {
             database.recipeDao().insert(recipe);
