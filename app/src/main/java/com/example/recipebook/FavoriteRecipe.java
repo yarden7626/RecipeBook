c020644b27b8d563e51f08cdcb3edeb9b0d10f5d
Zur <yarden7671@gmail.com>
2025-04-02 15:41:59 +0300

    הוספתי למסד נתונים טבלה ששומרת את המתכונים שבמועדפים אצל כל משתמש

--- app/src/main/java/com/example/recipebook/FavoriteRecipe.java
+++ app/src/main/java/com/example/recipebook/FavoriteRecipe.java
@@ -2,18 +2,23 @@ package com.example.recipebook;
 
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "favorite_recipes",
         foreignKeys = {
             @ForeignKey(entity = User.class,
-                    parentColumns = "userId",
+                    parentColumns = "id",
                     childColumns = "userId",
                     onDelete = ForeignKey.CASCADE),
             @ForeignKey(entity = Recipe.class,
                     parentColumns = "recipeId",
                     childColumns = "recipeId",
                     onDelete = ForeignKey.CASCADE)
+        },
+        indices = {
+            @Index("userId"),
+            @Index("recipeId")
         })
 public class FavoriteRecipe {
     @PrimaryKey(autoGenerate = true)
