9a2e617a4accb7d842c83eac5606c4a70f8a8f9f
Zur <yarden7671@gmail.com>
2025-05-15 15:23:21 +0300

    מחקתי דברים מיותרים שאין בהם שימוש

--- app/src/main/java/com/example/recipebook/Converters.java
+++ app/src/main/java/com/example/recipebook/Converters.java
@@ -7,17 +7,7 @@ import java.util.List;
 
 // מחלקה לביצוע המרות בין טיפוסי נתונים מורכבים לטיפוסים פשוטים שניתן לשמור בבסיס הנתונים
 public class Converters {
-    // המרה מערך מספרי לאובייקט תאריך
-    @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value);
-    }
 
-    // המרה מאובייקט תאריך לערך מספרי
-    @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
-    }
 
     // המרה מרשימת מחרוזות למחרוזת אחת מופרדת בפסיקים
     @TypeConverter
